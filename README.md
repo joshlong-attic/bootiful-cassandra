7a2b9f9328c9af8715a2bf2c1ce5d4867b4b56b3
clunven <cedrick.lunven@gmail.com>
2021-03-16 19:11:03 +0100

    Update with SDK

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
 # Getting Started with DataStax Astra for Cassandra 
 
-Specify a file path for   `cassandra.secure-connect-bundle` property. It should point to the secure connection bundle that you can download from the [astra.datastax.com](http://astra.datastax.com) portal in `Dashboard` -> `Databases` -> `<your keyspace>` -> `Connect`. Then, choose `Driver` and it'll let you `Download Secure Connect Bundle`. 
+
+## 1. Setup ASTRA
+
+- Create an Astra Instance
+
+- Create the Table
+
+```sql
+CREATE TABLE IF NOT EXISTS starter_orders (
+  order_id uuid,
+  product_id uuid,
+  product_quantity int,
+  product_name text,
+  product_price decimal,
+  added_to_order_at timestamp,
+  PRIMARY KEY ((order_id), product_id)
+ ) WITH CLUSTERING ORDER BY (product_id DESC);
+```
+
+- Create a token
+
+
+## 2. Build ASTRA SDK
+
+
+```bash
+git clone https://github.com/clun/astra-sdk-java.git
+cd stra-sdk-java
+mvn clean install -Dmaven.test.skip=true
+```
+
+
+## 3. Setup your application
+
+- Copy the db id, the cloud region and the apptoken in Application.properties
+
+```properties
+astra.cloudRegion=us-east-1
+astra.databaseId=de9d6c10-c8e6-43f0-95dc-72517cf801ec
+astra.applicationToken=AstraCS:lAkKZnycDWcAxCyOiBMdaoie:025dbbb5904xxx
+astra.keyspace=bootiful
+```
+
+## 4. Run TESTS
+
+
+- Start working with `AstraClientTest`
+
+```java
+should_list_available_db()
+```
+
+- Now notice in the `pom.xml`
+
+```xml
+<dependency>
+  <groupId>com.datastax.astra</groupId>
+  <artifactId>astra-spring-boot-starter</artifactId>
+  <version>2021.1-SNAPSHOT</version>
+</dependency>
+```
+
+- Run the Application
+
+```bash
+mvn spring-boot:run
+```
+
+## 5. Check in the DB
 
--- src/main/java/com/example/bootifulcassandra/BootifulCassandraApplication.java
+++ src/main/java/com/example/bootifulcassandra/BootifulCassandraApplication.java
@@ -28,88 +28,19 @@ import java.util.UUID;
 @SpringBootApplication
 public class BootifulCassandraApplication {
 
-
-
-
-	@Bean
-	ApplicationListener<ApplicationReadyEvent> ready(OrderRepository repository) {
-		return event -> {
-			var orderId = UUID.randomUUID();
-			var listOfOrders = new ArrayList<Order>();
-			for (var i = 0; i < 1000; i++) {
-				var productId = UUID.randomUUID();
-				listOfOrders.add(new Order(new OrderPrimaryKey(orderId, productId), 10,
-						"Product#" + productId, (float) (Math.random() * 100f), Instant.now()));
-			}
-
-			repository
-					.deleteAll()
-					.thenMany(repository.saveAll(listOfOrders))
-					.thenMany(repository.findAll())
-					.subscribe(System.out::println);
-
-		};
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(BootifulCassandraApplication.class, args);
 	}
 
-
-	@Bean
-	CqlSessionBuilderCustomizer cqlSessionBuilderCustomizer(@Value("${cassandra.secure-connect-bundle}") File file) {
-		Assert.isTrue(file.exists() , ()-> "specify a file path for the cassandra.secure-connect-bundle property. It should point to the secure connection " +
-				"bundle that you can downlaod from the astra.datastax.com portal in Dashboard -> Databases -> `bootiful` -> Connect. Then, choose Driver and it'll let you download a `Secure Connect Bundle` ");
-		return cqlSessionBuilder -> cqlSessionBuilder.withCloudSecureConnectBundle(file.toPath());
-	}
-
 }
 
 
 
 
-@Table(value = "starter_orders")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class Order implements Serializable {
-
-	@PrimaryKey
-	private OrderPrimaryKey key;
-
-	@Column("product_quantity")
-	@CassandraType(type = CassandraType.Name.INT)
-	private Integer productQuantity;
-
-	@Column("product_name")
-	@CassandraType(type = CassandraType.Name.TEXT)
-	private String productName;
-
-	@CassandraType(type = CassandraType.Name.DECIMAL)
-	@Column("product_price")
-	private Float productPrice;
-
-	@CassandraType(type = CassandraType.Name.TIMESTAMP)
-	@Column("added_to_order_at")
-	private Instant addedToOrderTimestamp;
-
-}
 
 
-@Repository
-interface OrderRepository extends ReactiveCassandraRepository<Order, OrderPrimaryKey> {
-}
 
 
-@PrimaryKeyClass
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class OrderPrimaryKey implements Serializable {
 
-	@PrimaryKeyColumn(name = "order_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
-	private UUID orderId;
 
-	@PrimaryKeyColumn(name = "product_id", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
-	private UUID productId;
-}
+
