ceee16916dc8baa60d6a6da9f7193c5d9820998a
Josh Long <josh@joshlong.com>
2021-03-18 09:13:15 -0700

    polish

--- src/main/java/com/example/bc1/Bc1Application.java
+++ src/main/java/com/example/bc1/Bc1Application.java
@@ -1,5 +1,8 @@
 package com.example.bc1;
 
+import com.datastax.oss.driver.api.core.DriverException;
+import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import com.datastax.oss.driver.api.core.cql.Row;
 import com.dstx.astra.sdk.AstraClient;
 import com.dstx.stargate.sdk.rest.TableClient;
 import lombok.AllArgsConstructor;
@@ -7,21 +10,29 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.reactivestreams.Publisher;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.ReactiveSession;
+import org.springframework.data.cassandra.ReactiveSessionFactory;
+import org.springframework.data.cassandra.core.cql.*;
 import org.springframework.data.cassandra.core.mapping.*;
 import org.springframework.data.cassandra.repository.Query;
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Log4j2
 @SpringBootApplication
@@ -32,17 +43,61 @@ public class Bc1Application {
 	}
 
 	@Bean
-	ApplicationRunner runner(AstraClient ac, CustomerOrdersRepository repository) {
+	ReactiveCqlTemplate reactiveCqlTemplate(ReactiveSessionFactory sessionFactory) {
+		return new ReactiveCqlTemplate(sessionFactory);
+	}
+
+	private final String dua = "Dua";
+	private final String patrick = "Patrick";
+	private final String mia = "Mia";
+	private final String josh = "Josh";
+
+
+	@Bean
+	ApplicationRunner runner(AstraClient ac,
+																										ReactiveCqlTemplate template,
+																										CustomerOrdersRepository repository) {
 		return event -> {
 
-			TableClient table = ac.apiRest().keyspace("crm").table("orders_by_customer");
-			 
+			Mono<Void> delete = repository.deleteAll();
 
-//			springData(repository, "Jane");
-//			documents(ac);
+			Flux<CustomerOrders> writes = Flux
+				.fromIterable(List.of(this.dua, this.mia, this.josh, this.patrick))
+				.flatMap(name -> addOrders(repository, name));
 
+			Flux<CustomerOrders> cql = this.cqlTemplate(template);
 
+			Flux<CustomerOrders> repoById = writes
+				.map(CustomerOrders::getCustomerId)
+				.take(1)
+				.flatMap(repository::findByCustomerId);
+
+			delete
+				.thenMany(writes)
+				.thenMany(repoById.doOnNext(co -> System.out.println("BY ID: " + co.toString())))
+				.thenMany(cql.doOnNext(co -> System.out.println("CQL: " + co.toString())))
+				.subscribe();
 		};
+	}
+
+
+	Flux<CustomerOrders> addOrders(CustomerOrdersRepository repository, String name) {
+
+		var customerId = UUID.randomUUID();
+		var list = new ArrayList<CustomerOrders>();
+
+		for (var i = 0; i < (Math.random() * 10); i++)
+			list.add(new CustomerOrders(customerId, UUID.randomUUID(), name));
+
+		return repository.saveAll(list);
+
+	}
+
+
+	Flux<CustomerOrders> cqlTemplate(ReactiveCqlTemplate template) {
+		return template
+			.query("select * from orders_by_customer ", (row, i) ->
+				new CustomerOrders(row.getUuid("customer_id"), row.getUuid("order_id"), row.getString("customer_name")));
 	}
 
 	private void documents(AstraClient ac) {
@@ -53,32 +108,6 @@ public class Bc1Application {
 			.create();
 	}
 
-
-	void springData(CustomerOrdersRepository repository, String name) {
-
-		var customerId = UUID.randomUUID();
-		var list = List.of(
-			new CustomerOrders(
-				customerId, UUID.randomUUID(), name),
-			new CustomerOrders(
-				customerId, UUID.randomUUID(), name),
-			new CustomerOrders(
-				customerId, UUID.randomUUID(), name)
-		);
-
-		/*repository
-			.saveAll(list)
-			.thenMany(repository.findAll())
-			.subscribe(System.out::println);
-		*/
-
-		repository
-			.saveAll(list)
-			.thenMany(repository.findByCustomerId(customerId))
-			.subscribe(System.out::println);
-
-
-	}
 
 }
 
@@ -131,21 +160,11 @@ class CustomerOrders {
 
 }
 
-/*@PrimaryKeyClass
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class OrderPrimaryKey {
-
-	@PrimaryKeyColumn(name = "order_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
-	private UUID orderId;
-
-	@PrimaryKeyColumn(name = "product_id", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
-	private UUID productId;
-}*/
 
 interface CustomerOrdersRepository extends ReactiveCassandraRepository<CustomerOrders, CustomerOrdersPrimaryKey> {
 
-	//	@Query("")
+
+	Flux<CustomerOrders> findByName(String name);
+
 	Flux<CustomerOrders> findByCustomerId(UUID customerId);
 }
